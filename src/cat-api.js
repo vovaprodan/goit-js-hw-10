96ee7fe4c335fb6127dff8710fe0cd5e5396df6a
vovaprodan <121820923+vovaprodan@users.noreply.github.com>
2023-06-10 23:16:39 +0200

    dz

---
+++ src/cat-api.js
@@ -0,0 +1,45 @@
+function fetchBreeds() {
+   return  fetch("https://api.thecatapi.com/v1/breeds")
+  .then(response => {
+   return response.json();
+  })
+     .then(data => {
+    const breedIds = data.map(breed => (
+     breed.id , breed.name
+    ))
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
+
+ 
+function fetchCatByBreed(breedId) {
+   fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=live_Zkffa83a5wqga5Z3mg9N1ZDEoOUSuy6ds2V0xPCqZmPPIwcuRmqJ7PlGY4VyH995`)
+  .then(response => {
+     return response.json();
+  })
+     .then(data => {
+       const divEl = document.querySelector('.cat-info')
+       console.log(data)
+       const catData = data[0];
+       const dataUrl = catData.url;
+       const breedName = catData.breeds[0].name;
+       const breedDescription = catData.breeds[0].description;
+      const breedTemperament = catData.breeds[0].temperament;
+       const markup = `<img src="${dataUrl}" alt="${breedName}" width='320'>
+            <h1 class="title">${breedName}</h1><p class="text">${breedDescription}</p>
+            <p class="text">Temperament: ${breedTemperament}</p>`
+       divEl.insertAdjacentHTML("afterbegin", markup);
+
+       
+     })
+     .catch(error => {
+       const errorEl = document.querySelector('.error')
+       errorEl.classList.remove('is-hedden')
+       Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    console.log(error)
+  });
+}
+
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const selectEl = document.querySelector('.breed-select')
+ const divEl = document.querySelector('.cat-info')
+const loaderEl = document.querySelector('.loader')
+const errorEl = document.querySelector('.error')
+
+errorEl.classList.add('is-hedden')
+selectEl.classList.add('is-hedden')
+
+
 
 fetch("https://api.thecatapi.com/v1/breeds")
   .then(response => {
      return response.json();
   })
-    .then(data => {
+  .then(data => {
+    selectEl.classList.remove('is-hedden')
+    loaderEl.classList.add('is-hedden')
         const cats = data.map((breed) => {
             const markup = ` <option value=${breed.id}>${breed.name}</option>`;
             selectEl.insertAdjacentHTML("afterbegin", markup);
         })
   })
   .catch(error => {
+     selectEl.classList.add('is-hedden')
+    errorEl.classList.remove('is-hedden')
+   Notify.failure('Oops! Something went wrong! Try reloading the page!');
     console.log(error)
   });
+
+selectEl.addEventListener("change",onChangeSelect)
+
+
+function onChangeSelect() {
+  
+  divEl.innerHTML = '';
+  const breedId = selectEl.value
+  fetchCatByBreed(breedId)
+  
+}
